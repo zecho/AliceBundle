926749be1c1b89f92246a6ffd2e9cbe9f5963e22
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-12-10 10:36:41 +0100

    Fix deprecated extends of ContainerAware

--- src/Doctrine/DataFixtures/AbstractLoader.php
+++ src/Doctrine/DataFixtures/AbstractLoader.php
@@ -12,16 +12,33 @@
 namespace Hautelook\AliceBundle\Doctrine\DataFixtures;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Helper for declaring doctrine data loaders.
  *
  * @author Baldur Rensch <brensch@gmail.com>
  * @author Théo FIDRY <theo.fidry@gmail.com>
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
  */
-abstract class AbstractLoader extends ContainerAware implements LoaderInterface
+abstract class AbstractLoader implements ContainerAwareInterface, LoaderInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritdoc}
+     *
+     * Use Symfony\Component\DependencyInjection\ContainerAwareTrait instead when dropping SF 2.3 and PHP 5.3 support.
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * Loads the fixtures files.
      *
